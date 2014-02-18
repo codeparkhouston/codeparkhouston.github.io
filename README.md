95dddc3cdda81e9d48651710f752730cdce16952
Hana Wang <h.wang081@gmail.com>
2014-02-17 20:23:22 -0600

    initial gh-pages commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### How to deploy
+1. Commit all your changes
+2. Push to Github
+3. rake build (assuming you have successfully bundled)
+4. rake publish
---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require _vendor/jquery/jquery
+//= require _vendor/jquery/jquery.easing.1.3
+//= require _vendor/jquery/jquery.parallax-1.1.3
+//= require _vendor/jquery/jquery.validate
+//= require _vendor/jquery/jquery.watermark.min
+//= require _vendor/jquery/waypoints.min
+//= require _vendor/jquery/theme
