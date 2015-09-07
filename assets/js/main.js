5f440aebd3dbfb06d90314faf4cf9344194b434b
pandafulmanda <pandafulmanda@gmail.com>
2015-09-07 15:56:06 -0500

    add quick email validation and units update

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,8 +48,14 @@ function handleForms(formsElements){
         if(input.value.length == 0){
           valid = false;
           input.parentNode.classList.remove('valid');
+          input.parentNode.classList.remove('not-email');
+        } else if(input.type == 'email' && !validateEmail(input.value)){
+          valid = false;
+          input.parentNode.classList.add('not-email');
+          input.parentNode.classList.remove('valid');
         } else {
           input.parentNode.classList.add('valid');
+          input.parentNode.classList.remove('not-email');
         }
       });
 
@@ -69,6 +75,13 @@ function handleForms(formsElements){
       }
     }
   }
+
+  // http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+  function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+  }
+
 }
 
 function handleActions(actionsElement){
